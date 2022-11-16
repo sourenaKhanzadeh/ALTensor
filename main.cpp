8035a6efd6b6a46199252cad1502752c77a8bff0
Sourena Khanzadeh <sourena.khanzadeh@ryerson.ca>
2022-11-16 17:22:00 -0500

    Added miscellanious

--- main.cpp
+++ main.cpp
@@ -7,15 +7,28 @@ int main() {
 
     ndarray<float> x({10, 1});
     ndarray<float> y({10, 1});
+    ndarray<float> theta({200, 1});
 
     for(int i=0; i<10; i++) {
-        x.set({i, 0}, i);
-        y.set({i, 0}, i*2);
+        if(i % 2 == 0) {
+            x.set({i, 0},i);
+            y.set({i, 0} , 1);
+        } else {
+            x.set({i, 0} , i);
+            y.set({i, 0} , 0);
+        }
     }
 
-    LinearRegression<float> lr(x, y);
+    for (int i = 0; i < 200; i++)
+    {
+        theta.set({i, 0}, i * 2);
+    }
+    
+
+    LogisticRegression<float> lr(x, y);
 
     lr.fit(10000, 0.0001);
+    std::cout << std::endl;
 
     std::cout << lr.getWeights() << std::endl;
     std::cout << lr.getBias() << std::endl;
@@ -23,9 +36,9 @@ int main() {
     std::cout << lr.predict(x).flatten() << std::endl;
     std::cout << y.flatten() << std::endl;
 
-    std::cout <<  "MSE: "<< lr.MSE() << std::endl;
+    std::cout <<  "Accuracy: "<< lr.accuracy() << std::endl;
     
-
+    std::cout << "predict(10): " << lr.predict(theta) << std::endl;
 
     return 0;
 }
